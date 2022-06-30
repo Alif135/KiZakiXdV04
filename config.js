e9ec404ec243de9dd47367a5052e74af3ef27972
Alif135 <61240413+Alif135@users.noreply.github.com>
2022-06-30 20:48:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,40 +3,40 @@ const chalk = require('chalk')
 
 global.thum = fs.readFileSync("./media/zaki.jpg")
 
-global.autoReadAll = true 
+global.autoReadAll = false 
 
-// '6285878313791' ganti nomor kalian '628xxxxxx'
-//KiZakiXD ganti nama kalian xxxxz
+// '6281315017916' ganti nomor kalian '628xxxxxx'
+//Khalif ganti nama kalian xxxxz
 //Silahkan sesuaikan sendiri ya
 
 // Ganti Sewajarnya
-global.owner = ['6285878313791']
-global.pengguna = 'KiZakiXD'
-global.premium = []
-global.author = 'KiZakiXD'
-global.pemilik = ['6285878313791'] 
-global.ownernomer = '6285878313791'
-global.ownername = 'KiZakiXD'
-global.botname = 'Chitanda - MD'
+global.owner = ['6281315017916']
+global.pengguna = 'Khalif'
+global.premium = ['6281315017916']
+global.author = 'Khalif'
+global.pemilik = ['6281315017916'] 
+global.ownernomer = '6281315017916'
+global.ownername = 'Khalif'
+global.botname = 'BOTz'
 global.tengah = "									"
 global.Prefix = 'Multi Prefix'
-global.meki = '6285878313791'
-global.footer = '𝘊 𝘩 𝘪 𝘵 𝘢 𝘯 𝘥 𝘢 - 𝘔 𝘋'
+global.meki = '6281315017916'
+global.footer = 'BOTz'
 //Isi Sesuai Namalu Buat Tambah Tqtq
-global.namalu = 'Namalu' //Ubah nama kalian buat nambahin di tqtq bot
+global.namalu = 'Khalif' //Ubah nama kalian buat nambahin di tqtq bot
 global.footer1 = 'Jangan Spam Bot.'
 global.footer2 = 'Jika Menemukan Bug Lapor Ke Owner.'
 global.yt = 'https://www.youtube.com/channel/UCHdipvk52k43KsPRenGpD0w' //Jika tidak ada yt jangan diubah buat promosi owner xixixixi
-global.ig = 'https://instagram.com/iamkizakixd'
+global.ig = 'https://instagram.com/khalif135.gm'
 global.gc = 'https://chat.whatsapp.com/IJ5ull4HXaY647D6ACw4kj' //join ya kadang kadang pada eval bot disitu lumayan dapet fitur baru wkwkwk
-global.dana = '00000'
+global.dana = '081315017916'
 global.gopay = '0000'
-global.wame = 'wa.me/6285878313791'
-global.email = 'helloiamkizakixd@gmail.com'
+global.wame = 'wa.me/6281315017916'
+global.email = 'khalif135gm@gmail.com'
 global.region = 'Indonesia'
 global.link = 'nekopoi.care'
 global.sc = 'https://bit.ly/SubscribeFLOB'
-global.packname = '"{\nAuthor: KiZakiXD"\n"Bot Number: 6285848376737"\n"Bot Name: Chitanda - MD"\n"Instagram: @iamkizakixd"\n}' 
+global.packname = '"{\nAuthor: Khalif"\n"Bot Number: 6283871618303"\n"Bot Name: BOTz"\n"Instagram: @khalif135.gm"\n}' 
 global.author = '' 
 global.sessionName = 'session'
 global.prefa = ['#','!','/','']
